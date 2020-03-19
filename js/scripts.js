369bd33f204e0e289e06868e4b8d271d0c1b437c
brandonhalvorsen <halvorsenbrandon@gmail.com>
2020-03-19 10:34:59 -0700

    Removed unused code

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,20 @@
-/*-----------------------------------------------------------------------------------
+/* ----------------------------------
 
-    Theme Name: Ashton - One Page Portfolio
-    Description: One Page Portfolio Template
-    Author: chitrakootweb
-    Version: 1.0
-
-    /* ----------------------------------
-
-    JS Active Code Index
-            
-        01. Preloader
-        02. scrollIt
-        03. Add Class Reveal for Scroll to Top
-        04. ScrollUp Active Code
-        05. Sidemenu toggle
-        06. navbar scrolling background
-        07. progress bar
-        08. sections background image from data background
-        09. Testimonials owlCarousel
-        10. magnificPopup
-        11. countUp
-        12. window When Loading
-        13. FullScreenHeight Resize function
-        14. CountDown for coming soon page
-
-        
-    ---------------------------------- */    
+JS Active Code Index
+    
+    Preloader
+    scrollIt
+    Add Class Reveal for Scroll to Top
+    ScrollUp Active Code
+    Sidemenu toggle
+    navbar scrolling background
+    sections background image from data background
+    magnificPopup
+    countUp
+    window When Loading
+    FullScreenHeight Resize function
+     
+---------------------------------- */    
 
 $(function() {
 
@@ -117,22 +106,6 @@ $(function() {
             $('.navbar-collapse.show').removeClass('show');
         });
       }
-   
-    // progress bar
-    wind.on('scroll', function () {
-        $(".skills-progress span").each(function () {
-            var bottom_of_object = 
-            $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = 
-            $(window).scrollTop() + $(window).height();
-            var myVal = $(this).attr('data-value');
-            if(bottom_of_window > bottom_of_object) {
-                $(this).css({
-                  width : myVal
-                });
-            }
-        });
-    });
 
     // sections background image from data background
     var pageSection = $(".bg-img, section");
@@ -142,18 +115,6 @@ $(function() {
             $(this).css("background-image", "url(" + $(this).data("background") + ")");
         }
     });
-
-    // === owl-carousel === //
-
-    // Testimonials owlCarousel
-    $('.testimonials .owl-carousel').owlCarousel({
-        items:1,
-        loop:true,
-        margin: 15,
-        autoplay:true,
-        smartSpeed:500
-    });
-
 
     // magnificPopup
     $('.gallery').magnificPopup({
@@ -228,26 +189,5 @@ $(function() {
     }
 
     SetResizeContent();
-
-    $(document).ready(function() {
-       
-        // Default owlCarousel
-        $('.owl-carousel').owlCarousel({
-            items: 1,
-            loop:true,
-            margin: 0,
-            autoplay:true,
-            smartSpeed:500
-        });
-
-        // CountDown for coming soon page
-            if ($(".countdown").length !== 0) {
-                $(".countdown").countdown({
-                    date: "01 Jan 2021 00:01:00", //set your date and time. EX: 15 May 2014 12:00:00
-                    format: "on"
-                });
-            }
-           
-    });
 
 });
